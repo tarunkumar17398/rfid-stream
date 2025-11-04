c6c485dccbbe879a4eb38a6269b6dc8a5067b049
tarunkumar17398 <87079669+tarunkumar17398@users.noreply.github.com>
2025-11-04 12:39:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
-const httpServer = createServer();
+const app = express();
+const httpServer = createServer(app);
+
 const io = new Server(httpServer, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*",
+    methods: ["GET","POST"],
+    allowedHeaders: ["*"]
+  },
+  transports: ["websocket", "polling"]
 });
+
+// required dummy route so Render thinks server is alive
+app.get("/", (req,res)=> res.send("OK"));
 
 io.on("connection", (socket) => {
   console.log("connected:", socket.id);
